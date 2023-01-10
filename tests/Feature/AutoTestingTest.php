07b991b2ca5d14703d5ff47b28cccdbb7afd26f0
Alexander Leahy <alex@languagetub.com>
2023-01-10 13:28:43 +1100

    Added failing test

---
+++ tests/Feature/AutoTestingTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AutoTestingTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_failing_test()
+    {
+        $this->assertTrue(false);
+    }
+}
